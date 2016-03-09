8b58c614f88eec06927d777e6b24fa3a10112207
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-03-09 01:01:27 -0500

    updated documentation

--- honeybeegrasshopper/test.py
+++ honeybeegrasshopper/test.py
@@ -2,10 +2,11 @@ from honeybee.radiance.sky.certainIlluminance import SkyWithCertainIlluminanceLe
 from honeybee.radiance.recipe.gridbased import HBGridBasedAnalysisRecipe
 from honeybee.hbsurface import HBAnalysisSurface
 
-pts = [(0, 0, 0), (10, 0, 0), (10, 10, 0), (0, 10, 0)]
-vectors = [(0, 0, 1)]
-sky = SkyWithCertainIlluminanceLevel(2000)
-hbsrf = HBAnalysisSurface("001", pts, surfaceType=None, isNameSetByUser=True, isTypeSetByUser=True)
-rp = HBGridBasedAnalysisRecipe(sky, pts, vectors, None, [hbsrf])
-rp.writeToFile("c:\ladybug", "test")
-rp.run(False)
+if __name__ == "__main__":
+	pts = [(0, 0, 0), (10, 0, 0), (10, 10, 0), (0, 10, 0)]
+	vectors = [(0, 0, 1)]
+	sky = SkyWithCertainIlluminanceLevel(2000)
+	hbsrf = HBAnalysisSurface("001", pts, surfaceType=None, isNameSetByUser=True, isTypeSetByUser=True)
+	rp = HBGridBasedAnalysisRecipe(sky, pts, vectors, None, [hbsrf])
+	rp.writeToFile("c:\ladybug", "test")
+	rp.run(False)
