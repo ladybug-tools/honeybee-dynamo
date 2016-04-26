b144dbbf6d0e9d1613aad662232d0e11b84ca7e9
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2016-04-25 20:38:25 -0400

    Added sunlighthours node
    This commit closes #10. It's pretty straight forward and quite easy to
    add new nodes thanks to honeybee implementation. runAnalysis node just
    works fine for this recipe and it will for any other recipe!

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-# from honeybeex.radiance.parameters.gridbased import GridBasedParameters
-#
-# radParameters = GridBasedParameters(0)
-# radParameters.ab = 20
-# radParameters.I = True
+from honeybeex.radiance.recipe.sunlighthours import SunlightHoursAnalysisRecipe
+
+sunVectors = [(0, 10, -10)]
+pointGroups = (0, 0, 0)
+vectorGroups = (0, 0, 1)
+rp = SunlightHoursAnalysisRecipe(sunVectors, pointGroups, vectorGroups,
+                                 timestep=1, ambientDivisions=1000, hbObjects=None)
+rp.writeToFile("c:\\ladybug",
+               "untitled2")
+rp.run(False)
+print rp.results()
