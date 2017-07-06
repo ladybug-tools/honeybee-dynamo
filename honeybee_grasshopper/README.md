1a0f967be5a1260543b873f97f42d18283a758fc
LAPTOP-MB0HU4QI\Mostapha <sadeghipour@gmail.com>
2017-07-05 23:18:15 -0400

    removed grasshopper related files
    see
    https://github.com/ladybug-tools/honeybee/commit/b95bec1c749a3a2ef2da412afcd787e2c6cc2628

--- honeybee_grasshopper/README.md
+++
@@ -1,4 +0,0 @@
-# honeybee_grasshopper
-Honeybee library for Grasshopper
-
-### [API Documentation](http://ladybug-analysis-tools.github.io/honeybee-plus/doc/honeybee_grasshopper/)
--- update_doc.bat
+++
@@ -1,5 +0,0 @@
-SET PYTHONPATH=C:\Users\Administrator\Documents\GitHub\honeybee;
-c:\Python27\python.exe C:\Python27\Scripts\pdoc --all-submodules --html --html-dir ./doc --overwrite honeybee_dynamo
-c:\Python27\python.exe C:\Python27\Scripts\pdoc --all-submodules --html --html-dir ./doc --overwrite honeybee_grasshopper
-
-pause
